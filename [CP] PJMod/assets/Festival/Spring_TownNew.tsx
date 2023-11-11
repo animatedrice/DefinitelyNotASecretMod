83479c3bb7e9d010d523ee712d44fa5132e1c09e
Eszter Csaki <esztercsaki3@gmail.com>
2023-11-11 22:02:42 +0100

    initial commit

---
+++ [CP] PJMod/assets/Festival/Spring_TownNew.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Spring_TownNew" tilewidth="16" tileheight="16" tilecount="2304" columns="32">
+ <image source="Spring_TownNew.png" width="512" height="1152"/>
+</tileset>
