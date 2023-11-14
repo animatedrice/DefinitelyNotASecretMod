892fcb9ac3642695fdc48efacf6af299858ebed7
evdm96 <elkevandemaat@hotmail.com>
2023-11-15 00:15:51 +0100

    map changes / festival fix

--- [CP] PJMod/assets/Festival/spring_outdoorsTileSheet1.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="spring_outdoorsTileSheet1" tilewidth="16" tileheight="16" tilecount="1975" columns="25">
- <image source="spring_outdoorsTileSheet1.png" width="400" height="1264"/>
-</tileset>
